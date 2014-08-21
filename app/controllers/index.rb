0fa91ffba7d13da1fe53362b56022943b75e51c2
ian <ianaroot@gmail.com>
2014-08-21 02:00:57 -0600

    dynamically find fields for attribute assignment

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,27 +7,48 @@ require 'ultrahook'
 require 'pry'
 
 post '/entries/create' do
+  # binding.pry
   args = {}
-  args[:time_avail] = params['Field10']
-  args[:urgency] = params['Field9']
-  args[:date] = params['DateCreated']
-  args[:email] = params['Field5']
-  args[:city] = params["Field2"]
-  if params['Field11'] == 'yes'
-    args[:remote_ok] = true
-  else
-    args[:remote_ok] = false
+  JSON.parse( params['FieldStructure'] )['Fields'].each do |field|
+    if field["Title"] == "your city"
+      args[:city] = params[field["ID"]]
+    elsif field["Title"] == "your first name"
+      args[:first_name] = params[field["ID"]]
+    elsif field["Title"] == "your last name"
+      args[:last_name] = params[field["ID"]]
+    elsif field["Title"] == "your request"
+      args[:comments]  = params[field["ID"]]
+    elsif field["Title"] == "your email"
+      args[:email] = params[field["ID"]]
+    elsif field["Title"] == "your phase"
+      args[:phase] = params[field["ID"]]
+    elsif field["Title"] == "timing"
+      args[:time_avail] = params[field["ID"]]
+    elsif field["Title"] == "is remote help okay?"
+      if params[field["ID"]] == 'yes'
+        args[:remote_ok] = true
+      else
+        args[:remote_ok] = false
+      end
+    end
   end
-  args[:phase] = params['Field7']
-  args[:comments] = params['Field8']
-  args[:name] = params['Field3']
+  args[:date] = params['DateCreated']
+  args[:name] = "#{args[:first_name]} #{args[:last_name]}"
+
+  # args[:time_avail] = params['Field10']
+  # args[:urgency] = params['Field9']
+  # args[:email] = params['Field5']
+  # args[:city] = params["Field2"]
+  # args[:phase] = params['Field7']
+  # args[:comments] = params['Field8']
+  # args[:name] = params['Field3']
   entry = Entry.new( args )
   entry.save
 end
 
 get '/' do
   #this is gonna change to San Francisco for the real app
-  @entries = Entry.where(city: "Sf")
+  @entries = Entry.all
   @entries = @entries.sort
   erb :index
 end
